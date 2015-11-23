f00188899779d178b29ad8c4a2741c7b79f67911
Jack Neely <jjneely@42lines.net>
2015-11-22 22:00:38 -0500

    Setup some locking primitives

---
+++ locking/locking.go
@@ -0,0 +1,42 @@
+package locking
+
+import (
+	"syscall"
+)
+
+func Exclusive(file *os.File) error {
+	if err := syscall.Flock(int(file.Fd()), syscall.LOCK_EX); err != nil {
+		return err
+	}
+	return nil
+}
+
+func Share(file *os.File) error {
+	if err := syscall.Flock(int(file.Fd()), syscall.LOCK_SH); err != nil {
+		return err
+	}
+	return nil
+}
+
+func TryExclusive(file *os.File) error {
+	lock := syscall.LOCK_EX | syscall.LOCK_NB
+	if err := syscall.Flock(int(file.Fd()), lock); err != nil {
+		return err
+	}
+	return nil
+}
+
+func TryShare(file *os.File) error {
+	lock := syscall.LOCK_SH | syscall.LOCK_NB
+	if err := syscall.Flock(int(file.Fd()), lock); err != nil {
+		return err
+	}
+	return nil
+}
+
+func Release(file *os.File) error {
+	if err := syscall.Flock(int(file.Fd()), syscall.LOCK_UN); err != nil {
+		return err
+	}
+	return nil
+}
