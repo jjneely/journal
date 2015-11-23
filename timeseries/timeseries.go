0388181d982fb4f64fabf4fb040ec2e7863f3044
Jack Neely <jjneely@42lines.net>
2015-11-22 22:01:23 -0500

    timeseries journal planning

--- timeseries/timeseries.go
+++ timeseries/timeseries.go
@@ -15,9 +15,13 @@ type Journal interface {
 	Width() int64
 
 	// Interval returns the number of time units (usually seconds) between
-	// each value.  If value at index 0 occurred at StartEpoch() then the
-	// value at index 3 occurred at StartEpoch() + Interval() * 3
+	// each value.
 	Interval() int64
+
+	// Meta returns the value stored in the int64 or 8 byte meta field
+	// in the header.  This can be used to recognize the data type stored
+	// in the Journal or represent other metadata.
+	Meta() int64
 
 	// Read locates the first value at the given Unix timestamp in the journal
 	// and will fill the provided []byte slice up to the slice length.
@@ -44,23 +48,51 @@ type Journal interface {
 }
 
 const (
-	journalMagic   string = "BCKY"
+	journalMagic   string = "BKTS"
 	journalVersion int32  = 0
 )
 
-func Open(path string) (Journal, error) {}
-
-func Create(path string) (Journal, error) {}
-
 type FileJournal struct {
-	epoch    int64
-	width    int64
-	interval int64
+	// File header format:  Magic identifier is 4 bytes
+	version  int32 // version is the next 4 bytes
+	width    int64 // width is the next 8 bytes
+	interval int64 // interval is the next 8 bytes
+	meta     int64 // meta is a type/metadata/maxsize field and 8 bytes
+	epoch    int64 // epoch is last in the header, and 8 bytes, if epoch
+	// is 0, there is no data in the file
 
 	fd      *os.File
-	version int32
+	datalen int64
 }
 
-func (ts *BuckyTS) StartEpoch() int64 {
+// Open finds the time series journal referenced by the given path, opens
+// the file and returns a FileJournal struct and any possible error.  Try to
+// open the underlying file read/write.  If that fails, open the file
+// read-only which means Write() calls will return an error.
+func Open(path string) (*FileJournal, error) {}
+
+// Create attempts to create a FileJournal at the given path, creating
+// any subdirectories needed by the path.  The width of the data type
+// that will be stored must be given.  A float64 is 8 bytes.  The
+// time units between each data point must also be given.  For a time
+// series file that records data points every 60 seconds must have interval
+// set to 60.  The meta parameter is a value defined by the application.
+func Create(path string, width, interval, meta int64) (*FileJournal, error) {
+
+}
+
+func (ts *FileJournal) Epoch() int64 {
 	return ts.epoch
+}
+
+func (ts *FileJournal) Meta() int64 {
+	return ts.meta
+}
+
+func (ts *FileJournal) Width() int64 {
+	return ts.width
+}
+
+func (ts *FileJournal) Interval() int64 {
+	return ts.interval
 }
